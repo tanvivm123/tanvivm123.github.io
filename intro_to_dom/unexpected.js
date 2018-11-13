a5706319f68ea2cb1f16f0579f5dabcbbc53cfe3
tanvivm123 <43019781+tanvivm123@users.noreply.github.com>
2018-11-13 02:05:22 -0500

    uplaoddd

---
+++ intro_to_dom/unexpected.js
@@ -0,0 +1,28 @@
+var clickable1 = document.getElementById("clickable1");
+var clickyThing1 = document.getElementById("clickyThing1");
+
+clickable1.onclick = function (){
+clickyThing1.classList.toggle("clicked1");
+}
+
+var clickable2 = document.getElementById("clickable2");
+var clickyThing2 = document.getElementById("clickyThing2");
+
+clickable2.onclick = function (){
+clickyThing2.classList.toggle("clicked2");
+}
+
+var clickable3 = document.getElementById("clickable3");
+var clickyThing3 = document.getElementById("clickyThing3");
+
+clickable3.onclick = function (){
+clickyThing3.classList.toggle("clicked3");
+}
+
+
+var clickable4 = document.getElementById("clickable4");
+var clickyThing4 = document.getElementById("clickyThing4");
+
+clickable4.onclick = function (){
+clickyThing4.classList.toggle("clicked4");
+}
